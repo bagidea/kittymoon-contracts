7e6fbfdd6beaa40c4ad533524cf032357ef9db06
bagidea <bagideachannel@gmail.com>
2022-11-01 06:07:21 +0700

    claim house finished

--- kittymoon-game/src/game.cpp
+++ kittymoon-game/src/game.cpp
@@ -1349,7 +1349,7 @@ ACTION game::claimland(
    auto it_land = lands.find(player_account.value);
    check(it_land != lands.end(), "not found land table from account");
 
-   if(slot_index == 0) check(it_land->lands.size() == 1, "can't claim land default");
+   if(slot_index == 0) check(it_land->lands.size() == 1, "can not claim land default");
    else check(it_land->lands.size() > 1, "you don't have lands staking");
 
    int8_t land_num = slot_index == 0 ? 0 : -1;
@@ -1383,12 +1383,10 @@ ACTION game::claimland(
    players.modify(
       it_player,
       get_self(),
-      [&](auto& s) {
-         s.energy -= use_energy;
-      }
+      [&](auto& s) { s.energy -= use_energy; }
    );
 
-   uint8_t i = stoi(it_land->lands[land_num].mining_bonus.substr(0, 1));
+   uint8_t i = stoi(it_land->lands[land_num].minting_bonus.substr(0, 1));
 
    while(i > 0) {
       action(
@@ -1443,8 +1441,8 @@ ACTION game::claimhouse(
    auto it_land = lands.find(player_account.value);
    check(it_land != lands.end(), "not found land table from account [house]");
 
-   if(slot_index == 0) check(it_land->lands.size() == 1, "can't house land default");
-   else check(it_land->lands.size() > 1, "you don't have lands staking");
+   if(slot_index == 0) check(it_land->lands.size() == 1, "can not claim house on land default");
+   else check(it_land->lands.size() > 1, "you don't have lands and house staking");
 
    int8_t land_num = slot_index == 0 ? 0 : -1;
 
@@ -1457,11 +1455,49 @@ ACTION game::claimhouse(
       }
    }
 
-   check(land_num > -1, "not found land in slot index");
-   check(now() - it_land->lands[land_num].current_time >= it_land->lands[land_num].cooldown_hr, "land can't claim, because cooldown not yet");
+   check(land_num > 0, "not found land in slot index [house]");
+   check(it_land->lands[land_num].house.asset_id != 0, "not found house in slot index");
+   check(now() - it_land->lands[land_num].house.current_time >= it_land->lands[land_num].house.cooldown_hr, "house can't claim, because cooldown not yet");
 
-   uint32_t use_energy = it_land->lands[land_num].energy_using;
+   uint32_t use_energy = it_land->lands[land_num].house.energy_using;
    check(it_player->energy >= use_energy, "player not enough energy");
+
+   lands.modify(
+      it_land,
+      get_self(),
+      [&](auto& s) { s.lands[land_num].house.current_time = now(); }
+   );
+
+   players.modify(
+      it_player,
+      get_self(),
+      [&](auto& s) { s.energy -= use_energy; }
+   );
+
+   uint8_t i = stoi(it_land->lands[land_num].house.minting_bonus.substr(0, 1));
+
+   while(i > 0) {
+      action(
+         permission_level {
+            get_self(),
+            "active"_n
+         },
+         ASSETS_ACCOUNT,
+         "mintasset"_n,
+         make_tuple(
+            get_self(),
+            config.get().ASSETS_COLLECTION_NAME,
+            config.get().ASSETS_SCHEMA_LANDS,
+            nfttemplates.get().tool_pack_template_id,
+            player_account,
+            map<string, ATOMIC_ATTRIBUTE>(),
+            map<string, ATOMIC_ATTRIBUTE>(),
+            vector<asset>()
+         )         
+      ).send();
+
+      i--;
+   }
 }
 
 ACTION game::receiverand(
